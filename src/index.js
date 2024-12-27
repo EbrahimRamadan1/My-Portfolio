de250760e7e1ae3947bcb87aaabc0defeea5adc8
EbrahimRamadan1 <clinton.hema@gmail.com>
2024-12-27 02:52:11 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -9,31 +9,7 @@ import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
-
-// #Essentials
-// npm i -D react-router-dom@latest
-// npm i sass
-// npm i bootstrap@latest
-// npm i axios
-// npm i vite-plugin-require
-
-// #State Management
-// npm install @reduxjs/toolkit react-redux
-// npm i recoil
-
-// #Fontawesome
-// npm i --save @fortawesome/fontawesome-svg-core
-// npm i --save @fortawesome/free-solid-svg-icons
-// npm i --save @fortawesome/free-regular-svg-icons
-// npm i --save @fortawesome/free-brands-svg-icons
-// npm i --save @fortawesome/react-fontawesome@latest
-
-// #Optional
-// npm i sweetalert2
-// npm i swiper
-// npm i wowjs
-// npm i animate.css --save
